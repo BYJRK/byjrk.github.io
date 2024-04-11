59e89bc5314ad8d5d3076369b021e8e2546d2c3b
BYJRK <suomikoto@outlook.com>
2024-04-11 13:38:00 +0800

    Improve Hugo-Server.ps1 script

--- Hugo-Server.ps1
+++ Hugo-Server.ps1
@@ -1,3 +1,8 @@
-Remove-Item -Recurse -Force .\resources
-Remove-Item -Recurse -Force .\public
-hugo server -D --disableFastRender
+if (Test-Path .\resources) {
+    Remove-Item -Recurse -Force .\resources
+}
+if (Test-Path .\public) {
+    Remove-Item -Recurse -Force .\public
+}
+
+Start-Process hugo -ArgumentList "server -D --disableFastRender" -NoNewWindow -Wait
